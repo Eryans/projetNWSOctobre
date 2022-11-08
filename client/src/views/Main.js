81f2ce61e3b0c92b22ae10f16a2397fa6238392e
DESKTOP-AJPRSU4\user <julesnoir@hotmail.fr>
2022-11-08 17:40:03 +0100

    Created DataTable component

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -5,26 +5,41 @@ import NavigationBar from "./components/NavigationBar";
 export default function Main() {
   const [data, setData] = useState();
   useEffect(() => {
-    getLoans().then((res) => setData(res));
+    try {
+      getLoans().then((res) => setData(res));
+    } catch (error) {
+      console.error(error);
+    }
   }, []);
   return (
     <>
       <NavigationBar />
-      <h1>Hello there</h1>
-      <div>
-        {data
-          ? data.loans.map((loan) => {
-              console.log(loan);
-              return (
-                <ul>
-                  <li>{loan.takenBy}</li>
-                  <li>{loan.createdAt}</li>
-                  <li>{loan.returnDate}</li>
-                </ul>
-              );
-            })
-          : ""}
-      </div>
+      <h1>Liste d'emprunts</h1>
+
+      {data ? <DataDable arrayToTreat={data.loans} /> : "Getting data"}
     </>
   );
 }
+
+const DataDable = ({ arrayToTreat }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {Object.keys(arrayToTreat[0]).map((key) => {
+            return key !== "_id" && key !== "__v" ? <th>{key}</th> : "";
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {arrayToTreat.map((data) => (
+          <tr>
+            {Object.keys(data).map((key) =>
+              key !== "_id" && key !== "__v" ? <td>{data[key]}</td> : ""
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
